57e3a11aa4cf5311c1a22fcc413a22b3d3106a61
Tima Omega <tima_omega@outlook.com>
2021-11-18 18:11:25 +0300

    commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 100,
+        simulateTouch: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+    });
+	$('.overlay').click(function () {
+		$(this).fadeOut();
+		$('.popup').fadeOut();
+		$("body").css("overflow", "auto")
+	});
+	$('.popup__close').click(function () {
+		$('.overlay').fadeOut();
+		$('.popup').fadeOut();
+		$("body").css("overflow", "auto")
+	});
+	$('.popup__trigger').click(function() {
+		$(this).next('.popup__info').slideToggle();
+		$(this).find('.popup__btn').toggleClass('active')
+	});
+	$('.form').validate({
+		focusCleanup: true,
+		rules: {
+            mail: {
+                required: false,
+                email: true,
+            },
+            surname: {
+                required: false,
+                minlength: 5,
+            },
+			num: {
+				number: true
+			},
+			name: {
+				minlength: 2,
+			},
+			comp: {
+				minlength: 2,
+			},
+			post: {
+				minlength: 2,
+			}
+        },
+		errorPlacement: function(error, element) {
+			return true;
+		}
+	})
+});
