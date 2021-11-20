5ceafc7aaa0d173449ad2ee0313dff61fca805cb
Tima Omega <tima_omega@outlook.com>
2021-11-20 08:00:58 +0300

    almost done

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-    const swiper = new Swiper('.swiper', {
+    const swiper = new Swiper('.video__slider', {
         slidesPerView: 'auto',
         centeredSlides: true,
         spaceBetween: 100,
         simulateTouch: false,
-		initialSlide: 1,
+        initialSlide: 1,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
@@ -14,52 +14,111 @@ $(document).ready(function () {
             type: 'fraction',
         },
     });
-	$('.video__slide').click(function() {
-		$('.overlay').fadeIn();
-		$('.popup').fadeIn();
-		$("body").css("overflow", "hidden")
-	});
-	$('.overlay').click(function () {
-		$(this).fadeOut();
-		$('.popup').fadeOut();
-		$("body").css("overflow", "auto")
-	});
-	$('.popup__close').click(function () {
-		$('.overlay').fadeOut();
-		$('.popup').fadeOut();
-		$("body").css("overflow", "auto")
-	});
-	$('.popup__trigger').click(function() {
-		$(this).next('.popup__info').slideToggle();
-		$(this).find('.popup__btn').toggleClass('active')
-	});
-	$('.form').validate({
-		focusCleanup: true,
-		rules: {
+
+    const swiper3 = new Swiper('.model__bottom', {
+        freeMode: true,
+        watchSlidesProgress: true,
+        slidesPerView: 'auto',
+        spaceBetween: 68,
+    });
+    const swiper2 = new Swiper('.model__slider', {
+        simulateTouch: false,
+        thumbs: {
+            swiper: swiper3,
+        },
+    });
+    $('.video__slide').click(function () {
+        $('.overlay').fadeIn();
+        $('.player').fadeIn();
+        $('body').css('overflow', 'hidden');
+    });
+    $('.model__btn').click(function () {
+        $('.overlay').fadeIn();
+        $('.popup').fadeIn();
+        $('body').css('overflow', 'hidden');
+    });
+    $('.overlay').click(function () {
+        $(this).fadeOut();
+        $('.popup').fadeOut();
+        $('.player').fadeOut();
+        $('.player__video').get(0).pause();
+        $('body').css('overflow', 'auto');
+    });
+    $('.popup__close').click(function () {
+        $('.overlay').fadeOut();
+        $('.popup').fadeOut();
+        $('.player').fadeOut();
+        $('.player__video').get(0).pause();
+        $('body').css('overflow', 'auto');
+    });
+    $('.popup__trigger').click(function () {
+        $(this).next('.popup__info').slideToggle();
+        $(this).find('.popup__btn').toggleClass('active');
+    });
+    $('.form').validate({
+        focusCleanup: true,
+        rules: {
             mail: {
                 email: true,
             },
             surname: {
-                minlength: 5,
+                minlength: 4,
+                lettersonly: true,
             },
-			num: {
-				number: true
-			},
-			name: {
-				minlength: 2,
-			},
-			comp: {
-				minlength: 2,
-			},
-			post: {
-				minlength: 2,
-			}
+            name: {
+                minlength: 2,
+                lettersonly: true,
+            },
+            comp: {
+                minlength: 4,
+            },
+            post: {
+                minlength: 4,
+                lettersonly: true,
+            },
+            num: {
+                digits: true,
+            },
         },
-		errorPlacement: function(error, element) {
-			return true;
-		}
-	})
-	$('.burger').click(function(){
-		$('.burger__list').toggleClass('active')
-	})
+        errorPlacement: function (error, element) {
+            return true;
+        },
+    });
+    jQuery.validator.addMethod('lettersonly', function (value, element) {
+        return this.optional(element) || /^[a-z]+$/i.test(value);
+    });
+
+    $('.burger').click(function () {
+        $('.burger__list').toggleClass('active');
+    });
+
+    wow = new WOW({
+        boxClass: 'animation',
+    });
+
+    wow.init();
+
+    function submit(e) {
+        e.preventDefault();
+        let formData = $(this).serializeArray(),
+            formUrl = $(this).attr('action'),
+            formMethod = $(this).attr('method');
+        formData.forEach(function (item) {
+            data.result[item.name] = item.value;
+        });
+        $.ajax({
+            url: formUrl,
+            type: formMethod,
+            dataType: 'JSON',
+            data: data.result,
+            success: function (response) {
+                let result = $.parseJSON(response);
+                console.log(result);
+                clear();
+            },
+            error: function () {
+                console.log('Ошибка. Данные не отправлены.');
+            },
+        });
+    }
 });
